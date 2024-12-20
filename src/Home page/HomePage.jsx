47f84a686863a5d13b77140bac002bf1ebd43b54
DattaPavanLovisetti <pavanlovisetti239@gmail.com>
2024-12-20 22:56:40 +0530

    Formatted Code

--- src/Home page/HomePage.jsx
+++ src/Home page/HomePage.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import TopBar from './Top Bar/TopBar'
-import "./Homepage.css"
-import LeftSideBar from './Left Side Bar/LeftSideBar'
-import RightSideBar from './Right Side Bar/RightSideBar'
-import Feed from './Feed/Feed'
+import React from "react";
+import TopBar from "./Top Bar/TopBar";
+import "./Homepage.css";
+import LeftSideBar from "./Left Side Bar/LeftSideBar";
+import RightSideBar from "./Right Side Bar/RightSideBar";
+import Feed from "./Feed/Feed";
 
 export default function HomePage() {
   return (
-    <div className='Homepage-main'>
-      <TopBar/>
-      <div className='mainWrapper'>
-      <LeftSideBar />
-      <Feed />
-      <RightSideBar />
+    <div className="Homepage-main">
+      <TopBar />
+      <div className="mainWrapper">
+        <LeftSideBar />
+        <Feed />
+        <RightSideBar />
       </div>
     </div>
-  )
+  );
 }
--- src/Home page/Left Side Bar/LeftSideBar.jsx
+++ src/Home page/Left Side Bar/LeftSideBar.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
-import "./LeftSideBar.css"
-import { Home , Explore, AddBox , PlayCircle, Settings, Notifications, Message} from '@mui/icons-material'
+import React from "react";
+import "./LeftSideBar.css";
+import {
+  Home,
+  Explore,
+  AddBox,
+  PlayCircle,
+  Settings,
+  Notifications,
+  Message,
+} from "@mui/icons-material";
 
 export default function LeftSideBar() {
   return (
-    <div className='leftsidebar-main'>
-      <div className='leftsidebar-icons'>
-      <Home className='LeftIcon' />
-        <span className='LeftIconName'>Home</span>
+    <div className="leftsidebar-main">
+      <div className="leftsidebar-icons">
+        <Home className="LeftIcon" />
+        <span className="LeftIconName">Home</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <Explore  className='LeftIcon'/>
-        <span className='LeftIconName'>Explore</span>
+      <div className="leftsidebar-icons">
+        <Explore className="LeftIcon" />
+        <span className="LeftIconName">Explore</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <AddBox className='LeftIcon' />
-        <span className='LeftIconName'>Create</span>
+      <div className="leftsidebar-icons">
+        <AddBox className="LeftIcon" />
+        <span className="LeftIconName">Create</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <PlayCircle className='LeftIcon' />
-        <span className='LeftIconName'>Videos</span>
+      <div className="leftsidebar-icons">
+        <PlayCircle className="LeftIcon" />
+        <span className="LeftIconName">Videos</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <Settings  className='LeftIcon'/>
-        <span className='LeftIconName'>Settings</span>
+      <div className="leftsidebar-icons">
+        <Settings className="LeftIcon" />
+        <span className="LeftIconName">Settings</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <Notifications className='LeftIcon' />
-        <span className='LeftIconName'>Notifications</span>
+      <div className="leftsidebar-icons">
+        <Notifications className="LeftIcon" />
+        <span className="LeftIconName">Notifications</span>
       </div>
-      <div className='leftsidebar-icons'>
-      <Message  className='LeftIcon'/>
-        <span className='LeftIconName'>Message</span>
+      <div className="leftsidebar-icons">
+        <Message className="LeftIcon" />
+        <span className="LeftIconName">Message</span>
       </div>
     </div>
-  )
+  );
 }
